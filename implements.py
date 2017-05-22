d73a5695f5610f2e1c664ab4bfa7cb9c38459a0d
Kamil Sindi <ksindi@ksindi.com>
2017-05-22 09:55:59 -0400

    various edits

--- implements.py
+++ implements.py
@@ -31,21 +31,25 @@ def implements(interface_cls, *, cache=True):
     def _decorator(cls):
         verify_methods(interface_cls, cls)
         verify_properties(interface_cls, cls)
+        # verify_attributes  # TODO
+        # verify_magic_methods  # TODO
+        # verify_descriptors  # TODO
+        # test other decorators are fine
         return cls
 
     return _decorator
 
 
 def verify_methods(interface_cls, cls):
-    method_predicate = lambda m: inspect.isfunction(m) or inspect.ismethod(m)
-    for name, method in inspect.getmembers(interface_cls, method_predicate):
-        interface_signature = inspect.signature(method)
+    methods_predicate = lambda m: inspect.isfunction(m) or inspect.ismethod(m)
+    for name, method in inspect.getmembers(interface_cls, methods_predicate):
+        signature = inspect.signature(method)
         cls_method = getattr(cls, name, None)
         cls_signature = inspect.signature(cls_method) if cls_method else None
-        if cls_signature != interface_signature:
+        if cls_signature != signature:
             raise NotImplementedError(
-                "Class '{}' must implement method '{}({})' defined in Interface '{}'."
-                .format(cls.__name__, name, interface_signature, interface_cls.__name__)
+                "Class '{}' must implement method '{}({})' defined in interface '{}'"  # flake8: noqa
+                .format(cls.__name__, name, signature, interface_cls.__name__)
             )
 
 
@@ -54,11 +58,9 @@ def verify_properties(interface_cls, cls):
     for name, prop in inspect.getmembers(interface_cls, inspect.isdatadescriptor):
         cls_prop = getattr(cls, name, None)
         for attr in prop_attrs:
-            # for some reason instanceof doesn't class function type comparison
-            interface_prop_type = type(getattr(prop, attr, None))
-            cls_prop_type = type(getattr(cls_prop, attr, None))
-            if interface_prop_type != cls_prop_type:
+            # instanceof doesn't work for class function comparison
+            if type(getattr(prop, attr, None)) != type(getattr(cls_prop, attr, None)):
                 raise NotImplementedError(
-                    "Class '{}' must implement a {} for property '{}' defined in interface '{}'."
+                    "Class '{}' must implement a {} for property '{}' defined in interface '{}'"  # flake8: noqa
                     .format(cls.__name__, prop_attrs[attr], name, interface_cls.__name__)
                 )
