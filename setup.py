a42f0ba7ecf596c0cf88c2f40b898bd61b40f43f
Kamil Sindi <ksindi@users.noreply.github.com>
2020-04-22 12:31:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,8 +40,9 @@ if __name__ == '__main__':
             License :: OSI Approved :: MIT License
             Natural Language :: English
             Programming Language :: Python :: 3
-            Programming Language :: Python :: 3.5
             Programming Language :: Python :: 3.6
+            Programming Language :: Python :: 3.7
+            Programming Language :: Python :: 3.8
             """).strip().splitlines(),
         keywords=['implements', 'interfaces'],
         license='MIT',
